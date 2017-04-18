d1c6aedb630cd452fff30b08ab562168bb46208f
Xavier Lepretre <xavier.lepretre@gmail.com>
2017-04-18 17:38:44 +0100

    Adding 1 EVM function.

---
+++ test/evmFunctions.js
@@ -0,0 +1,58 @@
+"use strict";
+
+const addEvmFunctions = require("../utils/evmFunctions.js");
+const chai = require('chai');
+chai.should();
+const expect = chai.expect;
+const sinon = require('sinon');
+const sinonChai = require("sinon-chai");
+chai.use(sinonChai);
+
+describe("EVM Functions", function() {
+    let web3;
+
+    beforeEach("should mock web3", function() {
+        web3 = {
+            currentProvider: {
+                sendAsync: sinon.stub()
+            }
+        }
+    });
+
+    it("should create evm object if absent", function() {
+        assert.isUndefined(web3.evm);
+        addEvmFunctions(web3);
+        assert.isDefined(web3.evm);
+    });
+
+    it("should create reuse evm object if present", function() {
+        web3.evm = {
+            pretendField: 1
+        };
+        addEvmFunctions(web3);
+        assert.isDefined(web3.evm);
+        assert.isDefined(web3.evm.increaseTime);
+        assert.strictEqual(web3.evm.pretendField, 1);
+    });
+
+    describe("increaseTime", function() {
+        beforeEach("should add EVM functions", function() {
+            addEvmFunctions(web3);
+        });
+
+        it("should pass parameter along", function() {
+            const callback = "callback1";
+            web3.evm.increaseTime(123, callback);
+            web3.currentProvider.sendAsync.should.have.been.calledOnce;
+            web3.currentProvider.sendAsync.should.have.been.calledWith(
+                {
+                    jsonrpc: "2.0",
+                    method: "evm_increaseTime",
+                    params: [ 123 ],
+                    id: sinon.match.number
+                },
+                "callback1");
+        });
+    });
+});
+
