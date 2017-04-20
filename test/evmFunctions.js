795f4762664b22a473dcd62ea6f5fac80e528040
Xavier Lepretre <xavier.lepretre@gmail.com>
2017-04-20 12:54:22 +0100

    Added snapshot and revert functions.

--- test/evmFunctions.js
+++ test/evmFunctions.js
@@ -36,6 +36,38 @@ describe("EVM Functions", function() {
             assert.strictEqual(web3.evm.pretendField, 1);
         });
 
+        it("should add snapshot function if absent", function() {
+            web3.evm = {};
+            assert.isUndefined(web3.evm.snapshot);
+            addEvmFunctions(web3);
+            assert.isDefined(web3.evm.snapshot);
+        });
+
+        it("should leave snapshot unchanged if present", function() {
+            web3.evm = {
+                snapshot: "snapshot1"
+            };
+            assert.isDefined(web3.evm.snapshot);
+            addEvmFunctions(web3);
+            assert.strictEqual(web3.evm.snapshot, "snapshot1");
+        });
+
+        it("should add revert function if absent", function() {
+            web3.evm = {};
+            assert.isUndefined(web3.evm.revert);
+            addEvmFunctions(web3);
+            assert.isDefined(web3.evm.revert);
+        });
+
+        it("should leave revert unchanged if present", function() {
+            web3.evm = {
+                revert: "revert1"
+            };
+            assert.isDefined(web3.evm.revert);
+            addEvmFunctions(web3);
+            assert.strictEqual(web3.evm.revert, "revert1");
+        });
+
         it("should add increaseTime function if absent", function() {
             web3.evm = {};
             assert.isUndefined(web3.evm.increaseTime);
@@ -50,6 +82,82 @@ describe("EVM Functions", function() {
             assert.isDefined(web3.evm.increaseTime);
             addEvmFunctions(web3);
             assert.strictEqual(web3.evm.increaseTime, "increaseTime1");
+        });
+    });
+
+    describe("snapshot", function() {
+        beforeEach("should add EVM functions", function() {
+            addEvmFunctions(web3);
+        });
+
+        it("should pass parameter along", function() {
+            const callback = "callback1";
+            web3.evm.snapshot(callback);
+            web3.currentProvider.sendAsync.should.have.been.calledOnce;
+            web3.currentProvider.sendAsync.should.have.been.calledWith(
+                {
+                    jsonrpc: "2.0",
+                    method: "evm_snapshot",
+                    params: [],
+                    id: sinon.match.number
+                },
+                sinon.match.func);
+        });
+
+        it("should return same if error", function() {
+            web3.currentProvider.sendAsync.yields("error1", "fakeResult1");
+            var callback = sinon.stub();
+            web3.evm.snapshot(callback);
+
+            callback.should.have.been.calledOnce;
+            callback.should.have.been.calledWith("error1", "fakeResult1");
+        });
+
+        it("should process return if ok", function() {
+            web3.currentProvider.sendAsync.yields(null, { result: "fakeResult1" });
+            var callback = sinon.stub();
+            web3.evm.snapshot(callback);
+
+            callback.should.have.been.calledOnce;
+            callback.should.have.been.calledWith(null, "fakeResult1");
+        });
+    });
+
+    describe("revert", function() {
+        beforeEach("should add EVM functions", function() {
+            addEvmFunctions(web3);
+        });
+
+        it("should pass parameter along", function() {
+            const callback = "callback1";
+            web3.evm.revert(123, callback);
+            web3.currentProvider.sendAsync.should.have.been.calledOnce;
+            web3.currentProvider.sendAsync.should.have.been.calledWith(
+                {
+                    jsonrpc: "2.0",
+                    method: "evm_revert",
+                    params: [ 123 ],
+                    id: sinon.match.number
+                },
+                sinon.match.func);
+        });
+
+        it("should return same if error", function() {
+            web3.currentProvider.sendAsync.yields("error1", "fakeResult1");
+            var callback = sinon.stub();
+            web3.evm.revert("fakeId1", callback);
+
+            callback.should.have.been.calledOnce;
+            callback.should.have.been.calledWith("error1", "fakeResult1");
+        });
+
+        it("should process return if ok", function() {
+            web3.currentProvider.sendAsync.yields(null, { result: "fakeResult1" });
+            var callback = sinon.stub();
+            web3.evm.revert("fakeId1", callback);
+
+            callback.should.have.been.calledOnce;
+            callback.should.have.been.calledWith(null, "fakeResult1");
         });
     });
 
--- utils/evmFunctions.js
+++ utils/evmFunctions.js
@@ -3,6 +3,39 @@
 module.exports = function(web3) {
     web3.evm = web3.evm ? web3.evm : {};
 
+    if (typeof web3.evm.snapshot === "undefined") {
+        /**
+         * @param {!function} callback - Node-type callback.
+         */
+        web3.evm.snapshot = function(callback) {
+            web3.currentProvider.sendAsync(
+                {
+                    jsonrpc: "2.0",
+                    method: "evm_snapshot",
+                    params: [],
+                    id: new Date().getTime()
+                },
+                (error, result) => callback(error, error ? result : result.result));
+        };
+    }
+
+    if (typeof web3.evm.revert === "undefined") {
+        /**
+         * @param {!number} snapshotId. The snapshot to revert.
+         * @param {!function} callback - Node-type callback.
+         */
+        web3.evm.revert = function(snapshotId, callback) {
+            web3.currentProvider.sendAsync(
+                {
+                    jsonrpc: "2.0",
+                    method: "evm_revert",
+                    params: [ snapshotId ],
+                    id: new Date().getTime()
+                },
+                (error, result) => callback(error, error ? result : result.result));
+        };
+    }
+
     if (typeof web3.evm.increaseTime === "undefined") {
         /**
          * @param {!number} offset. Time in milliseconds by which to advance the EVM.
